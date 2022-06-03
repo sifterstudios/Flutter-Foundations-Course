4a70293a7dc16f289b6ee094efb17af62137f4c0
Tormod Liseth <tormod@sifterstudios.no>
2022-06-03 22:36:16 +0200

    GoRouter Installation & Initial setup with MaterialApp.router

---
+++ lib/src/app.dart
@@ -0,0 +1,41 @@
+import 'package:ecommerce_app/src/features/products_list/products_list_screen.dart';
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final goRouter = GoRouter(initialLocation: '/', routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsListScreen(),
+      ),
+    ]);
+
+    return MaterialApp.router(
+      routerDelegate: goRouter.routerDelegate,
+      routeInformationParser: goRouter.routeInformationParser,
+      debugShowCheckedModeBanner: false,
+      restorationScopeId: 'app',
+      // * The home page of the app
+      onGenerateTitle: (BuildContext context) => 'My Shop'.hardcoded,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black87,
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.black, // background (button) color
+            onPrimary: Colors.white, // foreground (text) color
+          ),
+        ),
+      ),
+    );
+  }
+}
